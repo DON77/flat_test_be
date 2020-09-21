80f3813afc2a63310e9466aa768a9db741f45286
Armen <arm.sadoyan@gmail.com>
2020-09-21 21:28:09 +0400

    Fixed relationships attributes

--- app/Models/Relationship.php
+++ app/Models/Relationship.php
@@ -10,18 +10,4 @@ class Relationship extends Model
     use HasFactory;
 
     public $timestamps = false;
-
-    public function getAttributesAttribute() {
-        $this["attributes"] = [
-          'first_name' => $this->first_name,
-          'last_name' => $this->last_name,
-          'middle_name' => $this->middle_name
-        ];
-    }
-
-    protected $hidden = [
-        'first_name',
-        'last_name',
-        'middle_name',
-    ];
 }
